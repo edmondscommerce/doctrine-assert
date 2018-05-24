be3ce3c70a580cc88e55af29c699ca3106d474b9
Ben Rowan <ben.rob.rowan@gmail.com>
2018-05-24 20:42:25 +0100

    Add first simple tests

--- tests/SingleEntity/AssertDatabaseCountTest.php
+++ tests/SingleEntity/AssertDatabaseCountTest.php
@@ -23,20 +23,131 @@ class AssertDatabaseCountTest extends AbstractDoctrineTest
         return __DIR__ . '/Vfs';
     }
 
-    protected function loadFixtures()
+    public function testSettingNoQueryConfigReturnsAllResults(): void
     {
-        $generator = Factory::create();
-        $populator = new Populator($generator, $this->getEntityManager());
-        $populator->addEntity('Vfs\\SingleEntity\\Thing', 100);
-        $populator->execute();
-    }
+        $this->createSettingNoQueryConfigReturnsAllResultsFixtures();
 
-    public function testAssertsCorrectCount(): void
-    {
         $this->assertDatabaseCount(
             100,
             self::VFS_NAMESPACE . 'Thing',
             []
         );
+    }
+
+    private function createSettingNoQueryConfigReturnsAllResultsFixtures(): void
+    {
+        $generator = Factory::create();
+        $populator = new Populator($generator, $this->getEntityManager());
+
+        $populator->addEntity(self::VFS_NAMESPACE . 'Thing', 100);
+        $populator->execute();
+    }
+
+    public function testSettingSingleQueryConfigConstraintReturnsCorrectCount(): void
+    {
+        $this->createSettingSingleQueryConfigConstraintReturnsCorrectCountFixtures();
+
+        $this->assertDatabaseCount(
+            50,
+            self::VFS_NAMESPACE . 'Thing',
+            [
+                'active' => true
+            ]
+        );
+
+        $this->assertDatabaseCount(
+            50,
+            self::VFS_NAMESPACE . 'Thing',
+            [
+                'active' => false
+            ]
+        );
+    }
+
+    private function createSettingSingleQueryConfigConstraintReturnsCorrectCountFixtures(): void
+    {
+        $generator = Factory::create();
+        $populator = new Populator($generator, $this->getEntityManager());
+
+        $populator->addEntity(self::VFS_NAMESPACE . 'Thing', 50, [
+            'active' => function() { return false; }
+        ]);
+        $populator->execute();
+
+        $populator->addEntity(self::VFS_NAMESPACE . 'Thing', 50, [
+            'active' => function() { return true; }
+        ]);
+        $populator->execute();
+    }
+
+    public function testSettingDoubleQueryConfigConstraintReturnsCorrectCount(): void
+    {
+        $this->createSettingDoubleQueryConfigConstraintReturnsCorrectCountFixtures();
+
+        $this->assertDatabaseCount(
+            50,
+            self::VFS_NAMESPACE . 'Thing',
+            [
+                'active' => true,
+                'name'   => 'Aomame'
+            ]
+        );
+
+        $this->assertDatabaseCount(
+            50,
+            self::VFS_NAMESPACE . 'Thing',
+            [
+                'active' => true,
+                'name'   => 'Tamaru'
+            ]
+        );
+
+        $this->assertDatabaseCount(
+            50,
+            self::VFS_NAMESPACE . 'Thing',
+            [
+                'active' => false,
+                'name'   => 'Tengo'
+            ]
+        );
+
+        $this->assertDatabaseCount(
+            50,
+            self::VFS_NAMESPACE . 'Thing',
+            [
+                'active' => false,
+                'name'   => 'Eriko'
+            ]
+        );
+    }
+
+    private function createSettingDoubleQueryConfigConstraintReturnsCorrectCountFixtures(): void
+    {
+        $generator = Factory::create();
+        $populator = new Populator($generator, $this->getEntityManager());
+
+        $populator->addEntity(self::VFS_NAMESPACE . 'Thing', 50, [
+            'active' => function() { return true; },
+            'name'   => function() { return 'Aomame'; }
+        ]);
+        $populator->execute();
+
+        $populator->addEntity(self::VFS_NAMESPACE . 'Thing', 50, [
+            'active' => function() { return true; },
+            'name'   => function() { return 'Tamaru'; }
+        ]);
+        $populator->execute();
+
+        $populator->addEntity(self::VFS_NAMESPACE . 'Thing', 50, [
+            'active' => function() { return false; },
+            'name'   => function() { return 'Tengo'; }
+        ]);
+        $populator->execute();
+
+        $populator->addEntity(self::VFS_NAMESPACE . 'Thing', 50, [
+            'active' => function() { return false; },
+            'name'   => function() { return 'Eriko'; }
+        ]);
+        $populator->execute();
     }
 }

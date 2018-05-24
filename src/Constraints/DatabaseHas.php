fadfd5414f7f8190362848ce23a1f86b17926262
Ben Rowan <ben.rob.rowan@gmail.com>
2018-05-24 20:39:48 +0100

    Add improved failure messages

--- src/Constraints/DatabaseHas.php
+++ src/Constraints/DatabaseHas.php
@@ -52,12 +52,16 @@ class DatabaseHas extends AbstractDatabaseConstraint
     /**
      * Returns a description of the failure.
      *
-     * @param  string  $table
+     * @param  string  $rootEntityFqn
      * @return string
      */
-    public function failureDescription($table): string
+    public function failureDescription($rootEntityFqn): string
     {
-        return "FAIL - $table";
+        return sprintf(
+            "a '%s' entity matches the provided query config.\n\nQuery config:\n\n%s\n\n",
+            $rootEntityFqn,
+            $this->toString()
+        );
     }
 
     /**
